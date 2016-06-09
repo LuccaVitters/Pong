30f476354de23c5f90e5b9792bfd4779227058a6
Lucca <luvit023@gmail.com>
2016-06-09 11:09:16 +0200

    p2 physics

--- js/game.js
+++ js/game.js
@@ -17,8 +17,7 @@ var Akey;
 
 function create() {
     
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-    
+    game.physics.startSystem(Phaser.Physics.P2JS);
     
     
     ball = game.add.sprite(380, 280, 'ball');
@@ -27,19 +26,21 @@ function create() {
     paddleLeft = game.add.sprite(20, 300, 'paddleLeft');
     
     
-    paddleRight = game.add.sprite(980, 300, 'paddleRight');
+    paddleRight = game.add.sprite(960, 300, 'paddleRight');
     
     
-    game.physics.arcade.enable(ball);
+    game.physics.p2.enable(ball);
     ball.body.collideWorldBounds = true;
     ball.body.velocity.setTo(200,200);
     ball.body.bounce.set(1);
     
-    game.physics.arcade.enable(paddleLeft);
+    game.physics.p2.enable(paddleLeft);
     paddleLeft.body.collideWorldBounds = true;
+    paddleLeft.body.immovable = true;
     
-    game.physics.arcade.enable(paddleRight);
+    game.physics.p2.enable(paddleRight);
     paddleRight.body.collideWorldBounds = true;
+    paddleRight.body.immovable = true;
     
     cursors = game.input.keyboard.createCursorKeys();
 
@@ -50,8 +51,8 @@ function create() {
 
 function update() {
     
-    game.physics.arcade.collide(ball, paddleLeft, collisionHandlerPaddleLeft, null, this);
-    game.physics.arcade.collide(ball, paddleRight, collisionHandlerPaddleRight, null, this);
+    game.physics.p2.collide(ball, paddleLeft, collisionHandlerPaddleLeft, null, this);
+    game.physics.p2.collide(ball, paddleRight, collisionHandlerPaddleRight, null, this);
     
     paddleLeft.body.velocity.setTo(0, 0);
     paddleRight.body.velocity.setTo(0, 0);
@@ -80,9 +81,11 @@ function update() {
 function collisionHandlerPaddleLeft (ball, paddleLeft) {
     
     ball.body.velocity.y = -ball.body.velocity.y;
+    ball.body.velocity.x = -ball.body.velocity.x
 }
 
 function collisionHandlerPaddleRight (ball, paddleRight) {
     
     ball.body.velocity.y = -ball.body.velocity.y;
+    ball.body.velocity.x = -ball.body.velocity.x
 }
