c7c7fc7bf2757cb6e2bd9ba3b24d0f8a525c88be
Lucca <luvit023@gmail.com>
2016-06-21 13:49:47 +0200

    no message

--- js/map02.js
+++ js/map02.js
@@ -1,124 +1,173 @@
 var map02 = function(game){
     
-    var ball;
-    var paddle1;
-    var paddle2;
-    var Wkey, Akey;
-    var UpKey, DownKey;
-    var debugKey;
-    var ballMaterial;
-    var debug;
     var paddleSpeed;
-    var field;
     
+    var map;
+    var ball;
+    var player1, player2;
+    
+    var debugKey;
+    var debug;
 }
 
-map02.prototype = {
-
+map01.prototype = {
     preload: function () {
-
         this.game.load.image('ball', 'assets/ball.png');
         this.game.load.image('paddle', 'assets/paddle.png');
-        this.game.load.image("map02", "assets/map02.png");
-        this.game.load.physics("map02_physics", "assets/map02.json");
+        this.game.load.image("map_sprite", "assets/map02.png");
+        this.game.load.physics("map_physics", "assets/map02.json");
     },
 
-
-
-
     create: function () {
-        
-        debug = false;
         paddleSpeed = 200;
+        debug = false;
         
         this.game.physics.startSystem(Phaser.Physics.P2JS);
+        
+        //  turn on impact events for the world, without this we get no collision callbacks
+        this.game.physics.p2.setImpactEvents(true);
+        
+        // needed for ball mechanics
         this.game.physics.p2.restitution = 1;
         this.game.physics.p2.applyDamping = false;
-
-        ballMaterial = this.game.physics.p2.createMaterial();
+        var ballMaterial = this.game.physics.p2.createMaterial();
         this.game.physics.p2.createContactMaterial(ballMaterial, ballMaterial, {friction: 0, restitution: 1});
 
-        field = this.createField();
-        ball = this.createBall(500, 400, 200, 45);
-        paddle1 = this.createPaddle(230, 625, 90);
-        paddle2 = this.createPaddle(1050, 625, 90);
-
-        Wkey = this.game.input.keyboard.addKey(Phaser.KeyCode.D);
-        Skey = this.game.input.keyboard.addKey(Phaser.KeyCode.A);
-
-        UpKey = this.game.input.keyboard.addKey(Phaser.KeyCode.RIGHT);
-        DownKey = this.game.input.keyboard.addKey(Phaser.KeyCode.LEFT);
-
+        map = this.createMap();
+        ball = this.createBall(380, 150, 200, 45);
+        player1 = this.createPlayer(230, 500, -45, Phaser.KeyCode.W, Phaser.KeyCode.S, "goal1");
+        player2 = this.createPlayer(1050, 500, 45, Phaser.KeyCode.UP, Phaser.KeyCode.DOWN, "goal2");
+        
+        ball.sprite.body.collides(map.collisionGroup);
+        ball.sprite.body.collides(player1.paddle.collisionGroup);
+        ball.sprite.body.collides(player1.goal.collisionGroup);
+        ball.sprite.body.collides(player2.paddle.collisionGroup);
+        ball.sprite.body.collides(player2.goal.collisionGroup);
+        map.sprite.body.collides(ball.collisionGroup);
+        player1.paddle.sprite.body.collides(ball.collisionGroup);
+        player1.goal.sprite.body.collides(ball.collisionGroup);
+        player2.paddle.sprite.body.collides(ball.collisionGroup);
+        player2.goal.sprite.body.collides(ball.collisionGroup);
+        
         debugKey = this.game.input.keyboard.addKey(Phaser.KeyCode.TAB);
         debugKey.onDown.add(this.onDebugKeyDown, this);
     },
-
-    createField: function () {
-        var field = this.game.add.sprite(1280/2, 720/2, 'map02');
-        this.game.physics.p2.enable(field, this.debug);
-        field.body.clearShapes();
-        field.body.loadPolygon("map02_physics", "map02");
-        field.body.static = true;
-        return field;
+    
+    createMap: function () {
+        var sprite = this.game.add.sprite(1280/2, 720/2, "map_sprite");
+        this.game.physics.p2.enable(sprite);
+        sprite.body.clearShapes();
+        sprite.body.loadPolygon("map_physics", "map");
+        sprite.body.static = true;
+        
+        var collisionGroup = this.game.physics.p2.createCollisionGroup();
+        sprite.body.setCollisionGroup(collisionGroup);
+        
+        return {
+            sprite: sprite,
+            collisionGroup: collisionGroup
+        };
     },
-
+    
     createBall: function (x, y, speed, rotation) {
-        var ball = this.game.add.sprite(x, y, 'ball');
-        this.game.physics.p2.enable(ball, this.debug);
-        ball.body.rotation = rotation / 180 * Math.PI;
-        ball.body.moveForward(speed);
-        ball.body.setCircle(ball.width / 2);
-        return ball;
+        var sprite = this.game.add.sprite(x, y, "ball");
+        this.game.physics.p2.enable(sprite);
+        sprite.body.rotation = rotation / 180 * Math.PI;
+        sprite.body.moveForward(speed);
+        sprite.body.setCircle(sprite.width / 2);
+        
+        var collisionGroup = this.game.physics.p2.createCollisionGroup();
+        sprite.body.setCollisionGroup(collisionGroup);
+        
+        return {
+            sprite: sprite,
+            collisionGroup: collisionGroup
+        };
     },
-
+    
+    createPlayer: function (paddleX, paddleY, paddleRotation, forwardKey, backwardKey, goalKey) {
+        var player = {
+            paddle: this.createPaddle(paddleX, paddleY, paddleRotation),
+            goal: this.createGoal(goalKey),
+            controls: {
+                forwardKey: this.game.input.keyboard.addKey(forwardKey),
+                backwardKey: this.game.input.keyboard.addKey(backwardKey)
+            }
+        };
+        return player;
+    },
+    
     createPaddle: function (x, y, rotation) {
-        var paddle = this.game.add.sprite(x, y, 'paddle');
-        this.game.physics.p2.enable(paddle, this.debug);
-        paddle.body.kinematic = true;
-        paddle.body.rotation = rotation / 180 * Math.PI;
-        return paddle;
+        var sprite = this.game.add.sprite(x, y, "paddle");
+        this.game.physics.p2.enable(sprite);
+        sprite.body.kinematic = true;
+        sprite.body.rotation = rotation / 180 * Math.PI;
+        
+        var collisionGroup = this.game.physics.p2.createCollisionGroup();
+        sprite.body.setCollisionGroup(collisionGroup);
+        
+        return {
+            sprite: sprite,
+            collisionGroup: collisionGroup
+        };
+    },
+    
+    createGoal: function (key) {
+        var sprite = this.game.add.sprite();
+        this.game.physics.p2.enable(sprite);
+        sprite.body.clearShapes();
+        sprite.body.loadPolygon("map_physics", key);
+        sprite.body.static = true;
+        
+        var collisionGroup = this.game.physics.p2.createCollisionGroup();
+        sprite.body.setCollisionGroup(collisionGroup);
+        
+        return {
+            sprite: sprite,
+            collisionGroup: collisionGroup
+        };
+    },
+    
+    update: function () {
+        this.updatePlayer(player1);
+        this.updatePlayer(player2);
+    },
+        
+    updatePlayer: function (player) {
+        player.paddle.sprite.body.setZeroVelocity();
+        
+        if (player.controls.forwardKey.isDown) {
+            player.paddle.sprite.body.moveForward(paddleSpeed);
+        }
+        if (player.controls.backwardKey.isDown) {
+            player.paddle.sprite.body.moveBackward(paddleSpeed);
+        }
     },
 
+    render: function () {
+        if (debug) {
+            var ballSpeed = Math.sqrt(
+                Math.pow(ball.sprite.body.velocity.x, 2) + 
+                Math.pow(ball.sprite.body.velocity.y, 2));
+
+            this.game.debug.start(20, 20, 'white');
+            this.game.debug.line("Ball speed: " + ballSpeed);
+            this.game.debug.line("Ball angular speed: " + ball.sprite.body.angularVelocity);
+            this.game.debug.stop();
+        }
+    },
+    
     onDebugKeyDown: function () { 
         // toggle
         debug = !debug;
 
         // apply to bodies
-        ball.body.debug = debug;
-        paddle1.body.debug = debug;
-        paddle2.body.debug = debug;
-        field.body.debug = debug;
+        ball.sprite.body.debug = debug;
+        player1.paddle.sprite.body.debug = debug;
+        player2.paddle.sprite.body.debug = debug;
+        map.sprite.body.debug = debug;
 
         // apply to debug canvas
         this.game.debug.reset();
-    },
-
-    update: function () {
-        // paddle1
-        paddle1.body.setZeroVelocity();
-        if (Wkey.isDown) {
-            paddle1.body.moveForward(paddleSpeed);
-        } else if (Skey.isDown) {
-            paddle1.body.moveBackward(paddleSpeed);
-        }
-
-        // paddle2
-        paddle2.body.setZeroVelocity();
-        if (UpKey.isDown) {
-            paddle2.body.moveForward(paddleSpeed);
-        } else if (DownKey.isDown) {
-            paddle2.body.moveBackward(paddleSpeed);
-        }
-    },
-
-    render: function () {
-        if (this.debug) {
-            var ballSpeed = Math.sqrt(Math.pow(ball.body.velocity.x, 2) + Math.pow(ball.body.velocity.y, 2));
-
-            this.game.debug.start(20, 20, 'white');
-            this.game.debug.line("Ball speed: " + ballSpeed);
-            this.game.debug.line("Ball angular speed: " + ball.body.angularVelocity);
-            this.game.debug.stop();
-        }
     }
 }
